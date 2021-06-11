d4a1da11bbf9ff57b828b44ddf317b7a61208371
fernandiego <fernandodiego.sn@gmail.com>
2021-06-10 21:58:23 -0300

    rotas crud

--- route/postCertificado.js
+++ route/postCertificado.js
@@ -2,5 +2,24 @@ import express from 'express'
 const router = express.Router();
 
 
-router.get('/certificado/:id')
-router.post('/certificado')
+router.get('/certificados', async function (req, res) {
+	res.json([certificado]);
+});
+
+router.get('/certificados/:id', async function (req, res) {
+
+});
+
+router.post('/certificados', async function (req, res) {
+
+});
+
+router.put('/certificados/:id', async function (req, res) {
+
+});
+
+router.delete('/certificados/:id', async function (req, res) {
+
+});
+
+module.exports = router;
