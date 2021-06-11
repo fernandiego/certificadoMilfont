b45cdb7ac7e47706fc82bd5799ca699d7ef1106d
fernandiego <fernandodiego.sn@gmail.com>
2021-06-11 00:36:47 -0300

    rotas modularizadas, usando insomnia para testar req res

--- app/main.js
+++ app/main.js
@@ -1,24 +1,13 @@
-import express from 'express'
+const express = require('express')
+const {router} = require("../route/postCertificado");
+const bodyParser = require('body-parser')
 const app = express();
 
+app.use(bodyParser.json())
 
 
+app.use('/certificado',router )
 
-
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('hello world, lets go!');
-});
-
-// GET method route
-app.get('/', function (req, res) {
-  res.send('GET request to the homepage');
-});
-
-// POST method route
-app.post('/', function (req, res) {
-  res.send('POST request to the homepage');
-});
 
 app.listen(3000, () => {
   console.log('teste')
