ea3ae35edf13145473a22cfc475ce873666ec21c
fernandiego <fernandodiego.sn@gmail.com>
2021-06-13 00:28:45 -0300

    Finally, agora contar a história no readme

--- app/route/postCertificado.js
+++ app/route/postCertificado.js
@@ -7,20 +7,20 @@ const pup = require('puppeteer')
 
 
 router.post('/', async (req, res) => {
-	console.log("recebendo requisição")
+	// console.log("recebendo requisição")
 	// console.log(req.body)
 
 	axios.get(req.body.template).then(async ret => {
-		console.log('entrou axios')
+		// console.log('entrou axios')
 		let template = hb.compile(ret.data)
-		console.log('não explodiu template')
+		// console.log('não explodiu template')
 		// res.send(template(req.body.data))
-		const browser = await pup.launch();
-		console.log('não explodiu browser')
+		const browser = await pup.launch({headless: true});
+		// console.log('não explodiu browser')
 		const page = await browser.newPage();
-		console.log('não explodiu page')
+		// console.log('não explodiu page')
 		await page.setContent(template(req.body.data))
-		console.log('passou hb')
+		// console.log('passou hb')
 		const buffer = await page.pdf({
 			width: 1024,
 			height: 600,
@@ -32,10 +32,10 @@ router.post('/', async (req, res) => {
 				bottom: '0px'
 			}
 		})
-		console.log('gerou buffer pdf')
+		// console.log('gerou buffer pdf')
 		res.end(buffer)
 		await browser.close()
-		console.log('fechou browser')
+		// console.log('fechou browser')
 
 	})
 });
